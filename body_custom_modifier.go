b9fef37af9d009d6ed7fcfe413b3feb9c910eb13
asuscloud_henry <root@localhost.localdomain>
2020-09-08 18:05:15 +0800

    empty check2

--- body_custom_modifier.go
+++ body_custom_modifier.go
@@ -41,8 +41,7 @@ func (m *BodyModifier) ModifyRequest(req *http.Request) error {
 			data.Set(m.keys[i], req.Header.Get(m.target[i]))
 		}
 		req.Body = ioutil.NopCloser(strings.NewReader(data.Encode()))
-	}
-	else if m.source == "query" {
+	} else if m.source == "query" {
 		for i := 0; i < len(m.target); i++ {
 			if query.Get(m.target[i]) != "" {
 				data.Set(m.keys[i], query.Get(m.target[i]))
